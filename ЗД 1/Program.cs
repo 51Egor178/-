0a5741f5abd6870359832f99288c332ea5e80490
Egor <vev_113@mail.ru>
2024-01-30 20:53:02 +0300

    Задайте значения M и N

---
+++ ЗД 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNaturalNumbersInRange(m, n);
+    }
+
+    static void PrintNaturalNumbersInRange(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNaturalNumbersInRange(m + 1, n);
+        }
+    }
+}
