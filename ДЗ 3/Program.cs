531c2c02271f32325fe2ef9e75f53d8b0c470e26
Egor <vev_113@mail.ru>
2024-01-30 21:00:20 +0300

    произвольный массив

---
+++ ДЗ 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Произвольный массив
+        int[] myArray = { 1, 2, 3, 4, 5 };
+
+        // Вывод элементов массива, начиная с конца, с использованием рекурсии
+        PrintArrayReverse(myArray, myArray.Length - 1);
+
+        Console.ReadLine();
+    }
+
+    static void PrintArrayReverse(int[] array, int index)
+    {
+        // Базовый случай: если индекс меньше 0, выходим из рекурсии
+        if (index < 0)
+            return;
+
+        // Выводим текущий элемент массива
+        Console.Write(array[index] + " ");
+
+        // Рекурсивный вызов для следующего элемента с уменьшением индекса
+        PrintArrayReverse(array, index - 1);
+    }
+}
+
